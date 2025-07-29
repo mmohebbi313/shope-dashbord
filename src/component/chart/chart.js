7ca3dd68a2ef5818d73fdf8a23208d6ec025de47
m.mohebbi <m.mohebbi313313@gmail.com>
2025-07-29 07:24:07 -0700

    smalll

--- src/component/chart/chart.js
+++ src/component/chart/chart.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Legend, Tooltip , ResponsiveContainer } from 'recharts';
 import './chart.css'
+import { useEffect , useState } from "react";
 
 export default function Chart({title , grid , data , datakey}){
+    let [spect , setSpect] = useState(4)
+    let size = window.innerWidth 
+    useEffect( ()=> {
+       if(size <= 767){
+        setSpect(2)
+       }
+    }, [size])
+    
     return(
-        
         <div className="chart">
             <h3 className="h3">{title}</h3>
-            <ResponsiveContainer width="100%" aspect={4}>
+            <ResponsiveContainer width="97%" aspect={spect}>
         <LineChart data={data} >
             { grid && <CartesianGrid stroke="#aaa" strokeDasharray="10 10" />}
     <Line type="monotone" dataKey={datakey} stroke="#5550bd" strokeWidth={1} name="sale" />
