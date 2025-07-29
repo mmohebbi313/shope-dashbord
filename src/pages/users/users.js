53712d008dd29c724691b132b95a7ea8e8752d0d
m.mohebbi <m.mohebbi313313@gmail.com>
2025-07-29 02:14:27 -0700

    change button style

--- src/pages/users/users.js
+++ src/pages/users/users.js
@@ -44,46 +44,54 @@ export default function Users (){
 
  return(
 <div className="table-div1">
-<table className="table-div">
-    <tr className="tr-class">
-        <th className="th-class">ID</th>
-        <th className="th-class">User</th>
-        <th className="th-class">Email</th>
-        <th className="th-class">role</th>
-        <th className="th-class">password</th>
-        <th className="th-class-a">Action</th>
-    </tr>
-    {lastAction.map(data => (
-       <tr className="tr-user">
-       <td className="td-class">{data.id}</td>
-       <Link to='/' className="link">
-       <td className="td-user-class">
-           <img src={data.avatar} alt="my img" className="class-img"/>
-           {data.username}</td>
-           </Link>
-       <td className="td-class">{data.email}</td>
-       <td className="td-class">{data.role}</td>
-       <td className="td-class">{data.password}</td>
-       <td className="td-button-class">
-        <Link to = {`/user/${data.id}`} >
-           <button className="button-ed">
-           <MdEdit style={{color:"green" , fontSize:"17px"}}/>
-           </button>
-           </Link>
-           <button className="button-de" onClick={() => userDelete(data.id)}>
-           <MdDelete style={{color:"red" , fontSize:"17px"}}/>
-           </button>
-       </td>
-   </tr> 
-    ))}
+    {lastAction.length ? (
+        <table className="table-div">
+        <tr className="tr-class">
+            <th className="th-class">ID</th>
+            <th className="th-class">User</th>
+            <th className="th-class">Email</th>
+            <th className="th-class">Role</th>
+            <th className="th-class">Password</th>
+            <th className="th-class-a">Action</th>
+        </tr>
+        {lastAction.map(data => (
+           <tr className="tr-user">
+           <td className="td-class">{data.id}</td>
+           <Link to='/' className="link">
+           <td className="td-user-class">
+               <img src={data.avatar} alt="my img" className="class-img"/>
+               {data.username}</td>
+               </Link>
+           <td className="td-class">{data.email}</td>
+           <td className="td-class">{data.role}</td>
+           <td className="td-class">{data.password}</td>
+           <td className="td-button-class">
+            <Link to = {`/user/${data.id}`} >
+               <button className="button-ed">
+               <MdEdit style={{color:"green" , fontSize:"17px"}}/>
+               </button>
+               </Link>
+               <button className="button-de" onClick={() => userDelete(data.id)}>
+               <MdDelete style={{color:"red" , fontSize:"17px"}}/>
+               </button>
+           </td>
+       </tr> 
+        ))}
+    
+    </table>
+    ) : (
+        <h3 style={{color:"red", marginBottom:"10px"}}>Loading...</h3>
+    )}
 
-</table>
     <div className="div-pagein">
-    <button style={{display:"flex" , alignItems:"center" , justifyContent:"center"}} onClick={() => whatUserNR() }><MdNavigateBefore/></button>
+    <button className="buty" onClick={() => whatUserNR() }><MdNavigateBefore/></button>
     {pagecount.map(ff => (
-        <button style={{display:"flex" , alignItems:"center" , justifyContent:"center"}} onClick={() => whatUserNU(ff)}>{ff}</button>
+    ff > 0 && ff + 1 > number - 1 && ff + 1 < number + 3 && (
+        <button className={ ff == number ? "button-active"   : "buty" } onClick={() => whatUserNU(ff)}>{ff}</button> 
+
+    ) 
     ))}
-    <button style={{display:"flex" , alignItems:"center" , justifyContent:"center"}} onClick={() => whatUserNP() }><MdNavigateNext/></button>
+    <button className="buty" onClick={() => whatUserNP() }><MdNavigateNext/></button>
     </div>
 </div>
  );
