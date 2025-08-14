e5422239835fde36f747d5cfea1d37e4672cc4c8
Mohammad Mohebbi <m.mohebbi313313@gmail.com>
2025-08-14 12:05:59 +0330

    animating the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,35 @@ import { useRoutes } from 'react-router-dom';
 import Header from './component/header/header';
 import SideBar from './component/sidebar/sidebar';
 import React  , { useState } from "react";
+import { useRef , useEffect } from 'react';
 
 
 function App() {
   let [show , setShow] = useState(false)
   let importRoutes = useRoutes(MainRout)
+  let sidebarRef = useRef(null)
+
+useEffect(() => {
+    function handleClickOutside(event) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setShow(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+ 
   return (
+
    <div className='div'>
     <Header show={show} setShow={setShow}/>
     <div className='div-app'>
       { window.innerWidth <= 767 ? (
-      <div className={ show ? "show" : "side"}> 
+      <div ref={sidebarRef} className={ show ? "show" : "side"}> 
       <SideBar/>
       </div> 
       ) : (
